509408c1173f1cfe013c2bb1f16576028c217c3a
Hazel Tuxhorn <mx.jeckal@gmail.com>
2020-02-09 20:07:12 -0600

    layout and color scheme

--- script.js
+++ script.js
@@ -1,5 +1,15 @@
-var slider1 = document.getElementById('slider1')
-
-slider1.addEventListener("input", function() {
-    console.log('hmmm')
-})
+  // Tabbed Menu
+  function openMenu(evt, menuName) {
+      var i, x, tablinks;
+      x = document.getElementsByClassName("menu");
+      for (i = 0; i < x.length; i++) {
+          x[i].style.display = "none";
+      }
+      tablinks = document.getElementsByClassName("tablink");
+      for (i = 0; i < x.length; i++) {
+          tablinks[i].className = tablinks[i].className.replace(" w3-dark-grey", "");
+      }
+      document.getElementById(menuName).style.display = "block";
+      evt.currentTarget.firstElementChild.className += " w3-dark-grey";
+  }
+  document.getElementById("myLink").click();
